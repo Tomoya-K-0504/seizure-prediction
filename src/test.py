11a68ee68c6839ff4aae21409b43ff6a7c0c87c9
Tomoya Koike <makeffort134@gmail.com>
2019-05-14 09:57:12 +0900

    test modified

--- src/test.py
+++ src/test.py
@@ -1,61 +1,10 @@
 from __future__ import print_function, division
 
 import pandas as pd
-from args import test_args
-from eeglibrary import EEGDataSet, EEGDataLoader
-from eeglibrary.models.RNN import *
-from tqdm import tqdm
-from utils import *
+from eeglibrary.src import test
+from eeglibrary.utils import test_args
 
-def test(args, numpy, device):
-
-    eeg_conf = set_eeg_conf(args)
-    dataloader = set_dataloader(args, eeg_conf, phase='test', device=device)
-
-    model = set_model(args, eeg_conf, device)
-    if numpy:
-        model.load_model_(args.model_path)
-    else:
-        model.load_state_dict(torch.load(args.model_path))
-        model.eval()
-    pred_list = []
-    path_list = []
-
-    for i, (inputs, paths) in tqdm(enumerate(dataloader), total=len(dataloader)):
-        inputs = inputs.to(device)
-
-        if numpy:
-            preds = model.predict(inputs)
-        else:
-            outputs = model(inputs)
-            _, preds = torch.max(outputs, 1)
-        pred_list.extend(preds)
-        # Transpose paths, but I don't know why dataloader outputs aukward
-        path_list.extend([list(pd.DataFrame(paths).iloc[:, i].values) for i in range(len(paths[0]))])
-
-    def ensemble_preds(pred_list, path_list, sub_df, thresh):
-        # もともとのmatファイルごとに振り分け直す
-        patient_name = path_list[0][0].split('/')[-3]
-        orig_mat_list = sub_df[sub_df['clip'].apply(lambda x: '_'.join(x.split('_')[:2])) == patient_name]
-        ensembled_pred_list = []
-        for orig_mat_name in orig_mat_list['clip']:
-            seg_number = int(orig_mat_name[-8:-4])
-            one_segment_preds = [pred for path, pred in zip(path_list[0], pred_list) if
-                                 int(path.split('/')[-2].split('_')[-1]) == seg_number]
-            ensembled_pred = int(sum(one_segment_preds) >= len(one_segment_preds) * thresh)
-            ensembled_pred_list.append(ensembled_pred)
-        orig_mat_list['preictal'] = ensembled_pred_list
-        return orig_mat_list
-
-    # preds to csv
-    # sub_df = pd.read_csv('../output/sampleSubmission.csv')
-    sub_df = pd.read_csv(args.sub_path, engine='python')
-    thresh = args.thresh  # 1の割合がthreshを超えたら1と判断
-    pred_df = ensemble_preds(pred_list, path_list, sub_df, thresh)
-    sub_df.loc[pred_df.index, 'preictal'] = pred_df['preictal']
-    sub_df.to_csv(args.sub_path, index=False)
-
-    print_preds(sub_df)
+from src.utils import arrange_paths, class_names, subject_dir_names
 
 
 def print_preds(sub_df):
@@ -66,11 +15,6 @@ def print_preds(sub_df):
 
 if __name__ == '__main__':
     args = test_args().parse_args()
-    init_seed(args)
-    device = init_device(args)
-    if args.model_name in ['kneighbor', 'knn']:
-        args.model_name = 'kneighbor'
-    numpy = 'nn' not in args.model_name
 
     if args.only_results:
         sub_df = pd.read_csv(args.sub_path, engine='python')
@@ -80,6 +24,6 @@ if __name__ == '__main__':
     if args.test_manifest == 'all':
         for sub_name in subject_dir_names:
             args = arrange_paths(args, sub_name)
-            test(args, numpy, device)
+            test(args)
     else:
-        test(args, numpy, device)
+        test(args)
