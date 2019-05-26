19631e7e9e723cee7262303377f7d24b762eded7
Tomoya Koike <makeffort134@gmail.com>
2019-05-26 12:50:35 +0900

    adda after training available

--- src/train.py
+++ src/train.py
@@ -2,7 +2,7 @@ from __future__ import print_function, division
 
 from eeglibrary.src import train
 from eeglibrary.src import Metric
-from eeglibrary.utils import train_args
+from eeglibrary.utils import train_args, add_adda_args
 from utils import class_names, subject_dir_names, arrange_paths
 
 
@@ -11,11 +11,11 @@ def label_func(path):
 
 
 if __name__ == '__main__':
-    args = train_args().parse_args()
+    args = add_adda_args(train_args()).parse_args()
 
     metrics = [
         Metric('loss', initial_value=10000, inequality='less', save_model=True),
-        Metric('recall', initial_value=0, inequality='more'),
+        Metric('accuracy', initial_value=0, inequality='more'),
         Metric('far', initial_value=1000, inequality='less')]
 
     if args.train_manifest == 'all':
